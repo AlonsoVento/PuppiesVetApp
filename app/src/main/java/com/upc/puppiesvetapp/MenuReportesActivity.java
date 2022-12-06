9c92b9e7003b44f6315044d9f67f8a021065634f
lromerolev <lucrdtech@gmail.com>
2022-12-05 19:37:04 -0500

    Update MenuReportesActivity.java

--- app/src/main/java/com/upc/puppiesvetapp/MenuReportesActivity.java
+++ app/src/main/java/com/upc/puppiesvetapp/MenuReportesActivity.java
@@ -23,17 +23,38 @@ public class MenuReportesActivity extends AppCompatActivity {
         btn_ReporteVentasRealizadas=(Button)findViewById(R.id.btn_ReporteVentasRealizadas);
         btn_ReporteEstadoCompras=(Button)findViewById(R.id.btn_ReporteEstadoCompras);
 
-        btn_ReporteResidenciaClientes.setOnClickListener(new View.OnClickListener() {
+        btn_ReporteEstadoCliente.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 /*Intent intent = new Intent(MenuReportesActivity.this, ReporteResidenciaClientesActivity.class);
                 startActivity(intent);*/
-                Uri uri = Uri.parse( "https://app.powerbi.com/view?r=eyJrIjoiMTE0NmU5NDYtYTFkMS00ODMxLTkzMjctOGFmZjhmODYxNDk5IiwidCI6IjBlMGNiMDYwLTA5YWQtNDlmNS1hMDA1LTY4YjliNDlhYTFmNiIsImMiOjR9" );
+                Uri uri = Uri.parse( "https://app.powerbi.com/view?r=eyJrIjoiMTFlOTRlYTQtMDA1Yi00MzY5LWExM2YtZWIxY2FkZDA1ZDgzIiwidCI6IjBlMGNiMDYwLTA5YWQtNDlmNS1hMDA1LTY4YjliNDlhYTFmNiIsImMiOjR9&pageName=ReportSectiona7f4b95a781abc490023" );
                 Intent intent = new Intent(Intent.ACTION_VIEW,uri);
                 v.getContext().startActivity(intent);
             }
         });
 
+        btn_ReporteVentasRealizadas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*Intent intent = new Intent(MenuReportesActivity.this, ReporteResidenciaClientesActivity.class);
+                startActivity(intent);*/
+                Uri uri = Uri.parse( "https://app.powerbi.com/view?r=eyJrIjoiMTFlOTRlYTQtMDA1Yi00MzY5LWExM2YtZWIxY2FkZDA1ZDgzIiwidCI6IjBlMGNiMDYwLTA5YWQtNDlmNS1hMDA1LTY4YjliNDlhYTFmNiIsImMiOjR9" );
+                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                v.getContext().startActivity(intent);
+            }
+        });
+
+        btn_ReporteResidenciaClientes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*Intent intent = new Intent(MenuReportesActivity.this, ReporteResidenciaClientesActivity.class);
+                startActivity(intent);*/
+                Uri uri = Uri.parse( "https://app.powerbi.com/view?r=eyJrIjoiMTFlOTRlYTQtMDA1Yi00MzY5LWExM2YtZWIxY2FkZDA1ZDgzIiwidCI6IjBlMGNiMDYwLTA5YWQtNDlmNS1hMDA1LTY4YjliNDlhYTFmNiIsImMiOjR9&pageName=ReportSection" );
+                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                v.getContext().startActivity(intent);
+            }
+        });
 
     }
 }
